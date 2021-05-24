ccc1c8768bea458379c9b5e450504dec0f8ba972
Manuel Perez <manuelperezp22@hotmail.com>
2021-05-23 22:24:34 -0400

    Nuevo formulario request Anadiendo Picker de  Consulta Para los Estudiantes

---
+++ SCVPrueb2/SCVPrueb2/View/Request.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SCVPrueb2.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Request : ContentPage
+    {
+        public Request()
+        {
+            InitializeComponent();
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
